a9a4bf0034b64d868dfb891099a9c7f9bb1c8aa2
vivek <vivek.m@mindfiresolutions.com>
2016-06-11 16:28:13 +0530

    Added delete functionality to register.php

--- profile_app/register.php
+++ profile_app/register.php
@@ -15,8 +15,24 @@
 	    exit();
 	} 
 	
-	
-	if(!empty($_POST)) {
+	if ( $_GET["userAction"]=="delete" ) {
+		//Write a query to delete a row from the registration database with the respective employee id
+		$deleteAddress="DELETE FROM address WHERE eid=".$_GET["userId"].";";
+
+		$deleteCommMode="DELETE FROM commMedium WHERE empId=".$_GET["userId"].";";
+
+		$deleteEmployee="DELETE FROM employee WHERE eid=".$_GET["userId"].";";
+
+		mysqli_query($conn, $deleteAddress) or 
+					  header("Location:http://localhost/project/mindfire/profile_app/register.php?Message= delete failed:(");
+
+	    mysqli_query($conn, $deleteCommMode) or 
+					  header("Location:http://localhost/project/mindfire/profile_app/register.php?Message= delete failed:(");
+
+		mysqli_query($conn, $deleteEmployee) or 
+					  header("Location:http://localhost/project/mindfire/profile_app/register.php?Message= delete failed:(");
+	}
+	if ( !empty($_POST) ) {
 	
 		$prefix=$_POST["prefix"];
 
@@ -74,6 +90,7 @@
 						`employer`,`photo`,`note`)
 				VALUES ('$prefix','$firstName','$middleName','$lastName','$gender','$dob','$mobile','$landline','$email','$maritalStatus','$employment',
 						'$employer','$photo','$note')";
+				//Change here, make it a single if statement
 				if ($conn->query($query1) === TRUE) {
 					
 				    
@@ -83,7 +100,7 @@
 	   			    exit();
 				}
 				
-
+		//Change here , make a single query for both residencial and office address		
 		// insert residence address
 		$query2 = "INSERT INTO address (`eid`,`type`,`street`,`city`,`state`,`zip`,`fax`)
 				VALUES ('$empID','1','$residenceStreet','$resedenceCity','$resedenceState','$residenceZip','$residenceFax')";
@@ -95,6 +112,7 @@
 		// insert office address
 		$query3 = "INSERT INTO address (`eid`,`type`,`street`,`city`,`state`,`zip`,`fax`)
 				VALUES ('$empID','2','$officeStreet','$officeCity','$officeState','$officeZip','$officeFax')";
+				//Change here, make it a single if 
 				if ($conn->query($query3) === TRUE) {
 				    
 				} else {
@@ -111,12 +129,14 @@
 
 		$any=in_array("any", $commMedium) ? 1 : 0;
 
-		$query3 = "INSERT INTO commMedium (`empId`,`msg`,`email`,`call`,`any`)
+		$query4 = "INSERT INTO commMedium (`empId`,`msg`,`email`,`call`,`any`)
 				VALUES ('$empID','$msg','$comEmail','$call','$any')";
-				if ($conn->query($query3) === TRUE) {
+				//Change here,make it a sigle if
+				if ($conn->query($query4) === TRUE) {
 				    
 				} else {
-				    header("Location:http://localhost/project/mindfire/profile_app/registration_form.php?Message="." ".$conn->connect_error);
+				    header("Location:http://localhost/project/mindfire/profile_app/registration_form.php?Message="
+				    	  ." ".$conn->connect_error);
 	    			exit();
 				    
 				}
@@ -132,27 +152,27 @@
   
 </head>
 <body>
-<nav class="navbar navbar-default">
-	<div class="container">
-	    <div class="navbar-header">
-	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" 
-	        	aria-expanded="false" aria-controls="navbar">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-	      	</button>
-	     	<a class="navbar-brand" href="#">VIVEK</a>
-	    </div>
-	    <div id="navbar" class="navbar-collapse collapse">
-	      	<ul class="nav navbar-nav">
-		        <li><a href="http://localhost/project/mindfire/profile_app/registration_form.php">SIGN UP</a></li>
-		        <li><a href="#">LOG IN</a></li>
-		        <li><a href="http://localhost/project/mindfire/profile_app/register.php">DETAILS</a></li>
-	      	</ul>
-	    </div>
-	
-</nav>
+	<nav class="navbar navbar-default">
+		<div class="container">
+		    <div class="navbar-header">
+		        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" 
+		        	aria-expanded="false" aria-controls="navbar">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		      	</button>
+		     	<a class="navbar-brand" href="#">VIVEK</a>
+		    </div>
+		    <div id="navbar" class="navbar-collapse collapse">
+		      	<ul class="nav navbar-nav">
+			        <li><a href="http://localhost/project/mindfire/profile_app/registration_form.php">SIGN UP</a></li>
+			        <li><a href="#">LOG IN</a></li>
+			        <li><a href="http://localhost/project/mindfire/profile_app/register.php">DETAILS</a></li>
+		      	</ul>
+		    </div>
+		
+	</nav>
 
   
   <div class="row">
@@ -168,7 +188,8 @@
 					address.state , address.zip , address.fax 
 					FROM employee JOIN commMedium ON employee.eid = commMedium.empId
 					JOIN address ON  employee.eid = address.eid";
-				
+			
+			//Display some message when no data is present in the table	
 			$result = mysqli_query($conn, $query4) or 
 					  header("Location:http://localhost/project/mindfire/profile_app/registration_form.php?Message= :(");
 			
@@ -195,13 +216,13 @@
 	    </thead>
 	    <tbody>
 	    	<?php
-		    while($row = $result->fetch_assoc()) {
-		    	if($employeeId != $row["empId"]){
+		    while ($row = $result->fetch_assoc()) {
+		    	if ($employeeId != $row["empId"]) {
 			        echo "<tr>";
 
 			        echo "<td>".$row["firstName"]." ".$row["middleName"]." ".$row["lastName"]."</td>";
 
-			        if($row["gender"]=='m'){
+			        if ($row["gender"]=='m') {
 			        	echo "<td>Male</td>";
 			        }else if($row["gender"]=='f'){
 			        	echo "<td>Female</td>";
@@ -217,41 +238,47 @@
 
 			        echo "<td>".ucfirst($row["maritalStatus"])."</td>";
 
-			        if($row["employment"]=='employed'){
+			        if ($row["employment"]=='employed') {
 			        	echo "<td>".ucfirst($row["employment"])." in ".ucfirst($row["employer"])."</td>";			
 			        }else{
 			        	echo "<td>".ucfirst($row["employment"])."</td>";
 			        }
 			        echo "<td>";
 
-			        if($row["msg"]==1) {
+			        if ($row["msg"]==1) {
 			        	echo "Message";
 			        }
 
-			        if($row["comm_email"]==1) {
+			        if ($row["comm_email"]==1) {
 			        	echo "<br>Email";
 			        }
 
-			        if($row["call"]==1) {
+			        if ($row["call"]==1) {
 			        	echo "<br>Phone";
 			        }
 
-			        if($row["any"]==1) {
+			        if ($row["any"]==1) {
 			        	echo "<br>Any";
 			        }
 
 			        echo "</td>";
 		    	}
-		    	if($row["type"]==1){
+		    	if ($row["type"]==1) {
 		    		echo "<td>".$row["street"]."<br>".$row["city"].",".$row["zip"]."<br>".$row["state"]."</td>";
 		    	}	
 		        
-		        if($row["type"]==2){
+		        if ($row["type"]==2) {
 		    		echo "<td>".$row["street"]."<br>".$row["city"].",".$row["zip"]."<br>".$row["state"]."</td>";
+
+		    		echo "<td><a href='register.php?userId=".$row["eid"]."&userAction=update' target='_self' > update</a></td>";
+
+		    		echo "<td><a href='register.php?userId=".$row["eid"]."&userAction=delete' target='_self' > delete</a></td>";
+
 		    	}
-		        if($employeeId == $row["eid"]){
+		        if ($employeeId == $row["eid"]) {
 		      		echo "</tr>";
-		      	}	
+		      	}
+		      	
 			  $employeeId=$row["empId"];	 
 		    }
 		    ?>
@@ -261,7 +288,7 @@
   	</div>
   </div>
   
-</div>
+	</div>
 
 </body>
 </html>
